46bf97f47df41dd8f511c45975452fba50177bc7
NotMatta <xlenderboy666@gmail.com>
2024-07-31 15:26:25 +0100

    friendships established

--- app/api/user/follow/route.ts
+++ app/api/user/follow/route.ts
@@ -1,5 +1,42 @@
 import prisma from "@/prisma"
 import { handleQuery, ValidateToken } from "@/utils/apiUtils"
+import { NextRequest } from "next/server"
+
+const UpdateUserCount = async (userId:number) => {
+    const followerCount = await prisma.follow.count({where:{followingId:userId}})
+    const followingCount = await prisma.follow.count({where:{followerId:userId}})
+    await prisma.user.update({
+        data:{
+            followerCount,
+            followingCount
+        },
+        where:{
+            id: userId
+        }
+    })
+}
+
+const GET = async (req:NextRequest) => {
+    const tokenData : any = ValidateToken(req)
+    if (!tokenData){
+        return Response.json({message: "who tf are you"},{status: 403})
+    }
+    const targetUserId = Number(req.nextUrl.searchParams.get("targetUserId"))
+    if (!targetUserId || isNaN(targetUserId)){
+        return Response.json({message: "why?"},{status: 400})
+    }
+    if(targetUserId == tokenData.data.userId){
+        return Response.json({message: "Narcissit"},{status: 406})
+    }
+    const query = async () => {
+        const follow = await prisma.follow.findFirst({where: {
+            followerId: tokenData.data.userId,
+            followingId: targetUserId
+        }})
+        return follow ? {message:"",status: 200} : {message:"",status:404}
+    }
+    return await handleQuery(query)
+}
 
 const PUT = async (req:Request) => {
     const tokenData : any = ValidateToken(req)
@@ -10,16 +47,51 @@ const PUT = async (req:Request) => {
     if (!targetUserId || isNaN(targetUserId) || targetUserId == tokenData.data.userId){
         return Response.json({message: "why?"},{status: 400})
     }
+    if(targetUserId == tokenData.data.userId){
+        return Response.json({message: "Narcissit"},{status: 406})
+    }
     const query = async () => {
         await prisma.follow.create({data:{
-            followerId: Number(tokenData.data.userId),
+            followerId: tokenData.data.userId,
             followingId: Number(targetUserId)    
         }})
+        await UpdateUserCount(tokenData.data.userId)
+        await UpdateUserCount(Number(targetUserId))
         return {message:"Done!",status:201}
     }
     return await handleQuery(query)
 }
 
+const DELETE = async (req:NextRequest) => {
+    const tokenData : any = ValidateToken(req)
+    if (!tokenData){
+        return Response.json({message: "who tf are you"},{status: 403})
+    }
+    const targetUserId = Number(req.nextUrl.searchParams.get("targetUserId"))
+    if (!targetUserId || isNaN(targetUserId)){
+        return Response.json({message: "why?"},{status: 400})
+    }
+    if(targetUserId == tokenData.data.userId){
+        return Response.json({message: "Narcissit"},{status: 406})
+    }
+    const query = async () => {
+        const res = await prisma.follow.findFirst({where:{
+            followerId: tokenData.data.userId,
+            followingId: targetUserId   
+        }})
+        if(!res){
+            return {message:"Who again?",status:404}
+        }
+        await prisma.follow.delete({where:{id:res.id}})
+        await UpdateUserCount(tokenData.data.userId)
+        await UpdateUserCount(targetUserId)
+        return {message:"Friendship ended <3",status:201}
+    }
+    return await handleQuery(query)
+}
+
 export {
-    PUT
+    GET,
+    PUT,
+    DELETE
 }
--- app/main/profile/[id]/page.tsx
+++ app/main/profile/[id]/page.tsx
@@ -2,17 +2,54 @@
 import { RedirectorContext } from "@/components/redirector-provider"
 import { useSession } from "@/components/session-provider"
 import axios from "axios"
-import { MoveLeft } from "lucide-react"
+import { MoveLeft, Pen } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useContext, useEffect, useState } from "react"
+import Link from "next/link"
 
 const ProfilePage = ({params}: {params: {id: string}}) => {
 
     const session = useSession()
     const [load,setLoad] = useState(true)
     const [userInfo,setUserInfo] = useState({})
+    const [following,setFollow] = useState(false)
+    const [owner,setOwner] = useState(false)
     const setRedirector = useContext(RedirectorContext).setLoading
     const router = useRouter()
+
+    const triggerFollow = async () => {
+        setRedirector(true)
+        try{
+            await axios.put("/api/user/follow",{
+                targetUserId: params.id
+            },{
+                headers:{
+                    Authorization: `Bearer ${session.token}`
+                },
+            })
+            alert("Followed!")
+        }catch(err:any){
+            console.log(err.response)
+            alert("something happened!")
+        }
+        setLoad(true)
+    }
+    const triggerUnfollow = async () => {
+        setRedirector(true)
+        try{
+            await axios.delete("/api/user/follow",{
+                params:{targetUserId: params.id},
+                headers:{
+                    Authorization: `Bearer ${session.token}`
+                },
+            })
+            alert("Friendship ended!")
+        }catch(err:any){
+            console.log(err.response)
+            alert("something happened!")
+        }
+        setLoad(true)
+    }
 
     useEffect(() => {
         const FetchUser = async () => {
@@ -33,8 +70,26 @@ const ProfilePage = ({params}: {params: {id: string}}) => {
                 router.push("/main/home")
             }
         }
+        const FetchFollow = async () => {
+            try{
+                const res = await axios.get("/api/user/follow",{
+                    headers:{
+                        Authorization: `Bearer ${session.token}`
+                    },
+                    params:{
+                        targetUserId: params.id
+                    }
+                })
+                setFollow(true)
+            }catch(err:any){
+                err.response.status == 404 ? setFollow(false):
+                    err.response.status == 406? setOwner(true) : ""
+            }
+        }
         if (load && session.status == "authenticated"){
+            setRedirector(true)
             FetchUser()
+            FetchFollow()
             setLoad(false)
             setRedirector(false)
         }
@@ -46,7 +101,7 @@ const ProfilePage = ({params}: {params: {id: string}}) => {
                 <MoveLeft/>
                 <p>Go Back</p>
             </div>
-            {!load && <div className="w-full h-32 md:h-36 border-b flex items-end mb-16">
+            {!load && <div className="w-full h-32 md:h-36 border-b flex items-end mb-16 relative">
                 <div className="translate-y-1/2 flex gap-2">
                     <img src={userInfo?.pfp} className="rounded-full size-28 object-cover"/>
                     <div className="flex flex-col justify-between gap-1">
@@ -55,11 +110,23 @@ const ProfilePage = ({params}: {params: {id: string}}) => {
                             <p className="text-xs">{userInfo?.email}</p>
                         </div>
                         <div className="h-1/2">
-                            <p className="text-sm font-semibold">Followers: 0</p>
-                            <p className="text-sm font-semibold">Following: 0</p>
+                            <p className="text-sm font-semibold">Followers: {userInfo?.followerCount}</p>
+                            <p className="text-sm font-semibold">Following: {userInfo?.followingCount}</p>
                         </div>
                     </div>
                 </div>
+                {!owner && <div className="absolute right-0 box-border p-2">
+                        {!following && <button className="default p-2 text-sm" onClick={triggerFollow}>
+                                Follow
+                            </button>}
+                        {following && <button className="outlined p-2 text-sm" onClick={triggerUnfollow}>
+                                unfollow
+                            </button>}
+                            
+                    </div>}
+                {owner && <Link href="/main/settings" className="absolute right-0 flex box-border p-2 items-center gap-2 border rounded-xl m-2">
+                            <Pen className="size-4"/><p className="text-sm">Edit Profile</p>
+                    </Link>}
             </div>}
             <h3>Posts</h3>
         </div>
